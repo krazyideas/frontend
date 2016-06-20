fb9402c028929120975b2e008a36a5462e34370a
hongkailiu <hongkailiu@users.noreply.github.com>
2016-06-20 13:43:53 -0400

    add authentication before voting

--- routes/vote.js
+++ routes/vote.js
@@ -9,19 +9,26 @@ var voteUrl = "/vote";
 router.post('/', function(req, res, next) {
     console.log("url" + conf.backendUrl+voteUrl);
     console.log("body" + JSON.stringify(req.body));
+    console.log("req.isAuthenticated()" + req.isAuthenticated());
 
-    var options = {
-      uri: conf.backendUrl+voteUrl,
-      method: 'POST',
-      json: req.body
-    };
+    if (req.isAuthenticated()) {
+      var options = {
+        uri: conf.backendUrl+voteUrl,
+        method: 'POST',
+        json: req.body
+      };
 
-    request(options, function (error, response, body) {
-      console.log("sc: " + response.statusCode)
-      console.log("body: " + JSON.stringify(body))
-      res.statusCode = response.statusCode;
-      res.send(body);
-    });
+      request(options, function (error, response, body) {
+        console.log("sc: " + response.statusCode)
+        console.log("body: " + JSON.stringify(body))
+        res.statusCode = response.statusCode;
+        res.send(body);
+      });
+    } else {
+      res.statusCode = 401;
+      res.send("login before voting");
+    }
+
 
 });
 
